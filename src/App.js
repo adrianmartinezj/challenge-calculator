200a0e1f3cfc28e83902fca623bb25140cf3dbd2
adrianjustinmartinez@gmail.com <adrianjustinmartinez@gmail.com>
2019-09-25 14:52:55 -0700

    Init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './App.module.css';
+
+function App() {
+  return (
+    <div className={classes.App}>
+      <h1>Calculator Challenge</h1>
+    </div>
+  );
+}
+
+export default App;
